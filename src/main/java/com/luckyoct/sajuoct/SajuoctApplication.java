af499c3b248d3f7dab29153501451de29ed7824c
jungh8n <jh981113@naver.com>
2024-07-07 14:54:41 +0900

    initial commit

---
+++ src/main/java/com/luckyoct/sajuoct/SajuoctApplication.java
@@ -0,0 +1,13 @@
+package com.luckyoct.sajuoct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajuoctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SajuoctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckyoct/sajuoct/SajuoctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckyoct.sajuoct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajuoctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
