d530ccae92691a8e183bfa84bb7fb4382294820b
jungh8n <jh981113@naver.com>
2024-07-07 15:59:55 +0900

    add hello API

---
+++ src/main/java/com/luckyoct/sajuoct/HelloController.java
@@ -0,0 +1,14 @@
+package com.luckyoct.sajuoct;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, this is sajuoct server!";
+    }
+}
