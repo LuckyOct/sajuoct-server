d6149b685a39c1d321e0bcc435f579c3946b47ca
jungh8n <jh981113@naver.com>
2024-07-28 17:12:06 +0900

    conf: enable war build

--- src/main/java/com/luckyoct/sajuoct/SajuoctApplication.java
+++ src/main/java/com/luckyoct/sajuoct/SajuoctApplication.java
@@ -2,12 +2,19 @@ package com.luckyoct.sajuoct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SajuoctApplication {
+public class SajuoctApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SajuoctApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SajuoctApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SajuoctApplication.class, args);
+    }
 
 }
